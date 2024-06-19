6a7cddb8ce1d6e098dbe005cedc80e0b27885d22
daniel_santana_86 <daniel_santana_87@hotmail.com>
2024-06-18 22:17:53 -0300

    criação das pastas config, controller, database, model, services. Criação dos arquivos: config/db.php e  services/conexao.php

---
+++ database/webhospital_A00.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS webhospital;
+
+USE webhospital;
+
+CREATE TABLE IF NOT EXISTS tbl_pacientes(
+    id_paciente INT AUTO_INCREMENT PRIMARY KEY,
+    id_prontuario INT,
+    nome VARCHAR(30),
+    sobrenome VARCHAR(30),
+    email VARCHAR(150),
+    cep INT,
+    logradouro VARCHAR(150),
+    numero INT,
+    bairro VARCHAR(50),
+    cidade VARCHAR(100),
+    uf VARCHAR(2),
+    id_status INT,
+    CONSTRAINT fk_id_status FOREIGN KEY (id_status) REFERENCES tbl_status(id_status),
+    CONSTRAINT fk_id_prontuario FOREIGN KEY (id_prontuario) REFERENCES tbl_prontuarios(id_prontuario)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_cargos(
+    id_cargo INT AUTO_INCREMENT PRIMARY KEY,
+    descricao_cargo VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_prontuarios(
+    id_prontuario INT AUTO_INCREMENT PRIMARY KEY,
+    data_cadastro DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS tbl_status(
+    id_status INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(20)
+);
+
+
+CREATE TABLE IF NOT EXISTS tbl_procedimentos_exame(
+    id_procedimentos_exame INT AUTO_INCREMENT PRIMARY KEY,
+    id_tipos_procedimento INT,
+    CONSTRAINT fk_id_tipos_procedimento FOREIGN KEY (id_tipos_procedimento) REFERENCES tbl_tipos_procedimentos(id_tipos_procedimento)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_tipos_procedimentos(
+    id_tipos_procedimento INT AUTO_INCREMENT PRIMARY KEY,
+    descricao_procedimento VARCHAR(50) 
+);
